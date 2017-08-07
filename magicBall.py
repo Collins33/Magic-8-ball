0e7cdce6b602dc22b33ba8f711b2da74144f8ad4
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-07 18:50:38 +0300

    I have added the responses for the questions

--- magicBall.py
+++ magicBall.py
@@ -1,3 +1,12 @@
+import random
 print("Enter any life questions you have:")
 question =input()
-print (question)
+number =random.randint(0,3)
+if number == 0:
+    print("Don't count on it")
+elif number == 1:
+    print("Signs point to yes")
+elif number ==2:
+    print("Better not tell you now")
+else:
+    print("Most likely")            
